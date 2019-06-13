4791dce46a42887e997ef7563c2adf77d309c6b5
rexjoseph <josephrex96@gmail.com>
2019-06-13 23:34:23 +0100

    add redirects to eacj resource

--- js/app.js
+++ js/app.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 let mainNav = document.querySelector('#js-menu');
     let navBarToggle = document.querySelector('#js-navbar-toggle');
 
     navBarToggle.addEventListener('click', () => {
       mainNav.classList.toggle('active');
-=======
-let mainNav = document.querySelector('#js-menu');
-    let navBarToggle = document.querySelector('#js-navbar-toggle');
-
-    navBarToggle.addEventListener('click', () => {
-      mainNav.classList.toggle('active');
->>>>>>> upstream/master
     });
